a4bd190a1077f0f446810b6dfacafbeae155eb1d
Rakshitha Prasad <126645011+rakshithaprasad204@users.noreply.github.com>
2024-03-31 22:54:39 +0530

    Testing.py

--- StreamlitDemo/Testing.py
+++ StreamlitDemo/Testing.py
@@ -1,15 +1,5 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
-
-#Tensorflow Model Prediction
-def model_prediction(test_image):
-    model = tf.keras.models.load_model("trained_model.h5")
-    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(64,64))
-    input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr]) #convert single image to batch
-    predictions = model.predict(input_arr)
-    return np.argmax(predictions) #return index of max element
 
 st.sidebar.title("Dashboard")
 app_mode = st.sidebar.selectbox("Select Page",["Home","About Project","Prediction"])
@@ -36,15 +26,3 @@ elif(app_mode=="Prediction"):
     test_image = st.file_uploader("Choose an Image:")
     if(st.button("Show Image")):
         st.image(test_image,width=4,use_column_width=True)
-    #Predict button
-    if(st.button("Predict")):
-        st.snow()
-        st.write("Our Prediction")
-        result_index = model_prediction(test_image)
-        #Reading Labels
-        with open("labels.txt") as f:
-            content = f.readlines()
-        label = []
-        for i in content:
-            label.append(i[:-1])
-        st.success("Model is Predicting it's a {}".format(label[result_index]))
